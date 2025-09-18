d967fb2e34ddbb9fad03115b2f636c9699911c34
wilmermendezcodes <WilmerMendez500@outlook.com>
2025-09-19 01:15:20 +0800

    3rd commit - patch 1

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,19 +1,19 @@
 const request = require("request");
 const dotenv = require("dotenv");
 const path = require("path");
-const weatherStackURL = "http://api.weatherstack.com/current";
-const geoApifyURL = "https://api.geoapify.com/v1/geocode/search";
 
 dotenv.config({
   // Load the .env placed at the repo root (two levels up from this file)
   path: path.resolve(__dirname, "../../.env"),
 });
 console.log(process.env.GEOAPIFY_KEY);
-const { GEOAPIFY_KEY, WEATHERSTACK_KEY } = process.env;
+
+const { GEOAPIFY_URL: geoApifyURL, GEOAPIFY_KEY: geoApifyKey } = process.env;
+const { WEATHERSTACK_URL: weatherStackURL, WEATHERSTACK_KEY: weatherStackKey } = process.env;
 
 (async () => {
     const { latitude, longitude } = await fetchCoordinates("Felisa, Bacolod");
-    const url = `${weatherStackURL}?access_key=${WEATHERSTACK_KEY}&query=${ latitude },${ longitude }&units=m`;
+    const url = `${weatherStackURL}?access_key=${weatherStackKey}&query=${latitude},${longitude}&units=m`;
     request({ url: url, json: true }, (error, response) => {
         console.log(url);
         if (error) {
@@ -38,13 +38,13 @@ const { GEOAPIFY_KEY, WEATHERSTACK_KEY } = process.env;
 
 
 function fetchCoordinates(loc) {
-    const url = `${geoApifyURL}?text=${encodeURIComponent(loc)}&apiKey=${GEOAPIFY_KEY}`;
+    const url = `${geoApifyURL}?text=${encodeURIComponent(loc)}&apiKey=${geoApifyKey}`;
     console.log(url);
     return new Promise((resolve, reject) => {
         request({ url: url, json: true }, (error, response) => {
             if (error) {
                 reject("Unable to connect to location services!");
-            } else if (!response.body.features) {
+            } else if (response.body.features.length === 0) {
                 reject("Unable to find location. Try another search.");
             } else {
                 const data = response.body.features[0];
